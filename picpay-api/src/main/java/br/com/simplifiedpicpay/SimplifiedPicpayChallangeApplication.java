3fb9163439c09bf787db2dbe1c50b3b7b20d3d77
Vítor Lougon <vitor.lougon@gmail.com>
2025-11-07 18:40:11 -0300

    feat: initializing project

---
+++ picpay-api/src/main/java/br/com/simplifiedpicpay/SimplifiedPicpayChallangeApplication.java
@@ -0,0 +1,15 @@
+package br.com.simplifiedpicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPicpayChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPicpayChallangeApplication.class, args);
+
+		System.out.println("Test");
+	}
+
+}
