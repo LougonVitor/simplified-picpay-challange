3da4a90c55f931742564edeca64188ff746d8639
Vítor Lougon <vitor.lougon@gmail.com>
2025-11-08 15:02:12 -0300

    feat: creating bounded notification context

---
+++ picpay-api/src/main/java/br/com/simplifiedpicpay/notification/dto/NotificationRequestDto.java
@@ -0,0 +1,4 @@
+package br.com.simplifiedpicpay.notification.dto;
+
+public record NotificationRequestDto(String email, String message) {
+}
---
+++ picpay-api/src/main/java/br/com/simplifiedpicpay/notification/services/NotificationService.java
@@ -0,0 +1,27 @@
+package br.com.simplifiedpicpay.notification.services;
+
+import br.com.simplifiedpicpay.notification.dto.NotificationRequestDto;
+import br.com.simplifiedpicpay.user.domain.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(String email, String message) throws Exception {
+        NotificationRequestDto notificationRequestDto = new NotificationRequestDto(email, message);
+
+        ResponseEntity<String> apiResponse = this.restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequestDto, String.class);
+
+        boolean isNotified = (apiResponse.getStatusCode() == HttpStatus.OK);
+        if(!isNotified) {
+            System.out.println("Error when sending notification");
+            throw new Exception("Notification service is down.");
+        }
+    }
+}
