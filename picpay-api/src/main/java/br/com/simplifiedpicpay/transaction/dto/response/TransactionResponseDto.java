0a06360c9904cd3e1526bbfbe040df799b515a52
Vítor Lougon <vitor.lougon@gmail.com>
2025-11-08 13:59:13 -0300

    fix: adjusting the dto transaction to receive only values, not classes

--- picpay-api/src/main/java/br/com/simplifiedpicpay/transaction/dto/response/TransactionResponseDto.java
+++ picpay-api/src/main/java/br/com/simplifiedpicpay/transaction/dto/response/TransactionResponseDto.java
@@ -5,5 +5,5 @@ import br.com.simplifiedpicpay.user.domain.model.User;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record TransactionResponseDto (Long id, BigDecimal amount, User sender, User receiver, LocalDateTime timestamp){
+public record TransactionResponseDto (Long id, BigDecimal amount, Long senderId, Long receiverId){
 }
