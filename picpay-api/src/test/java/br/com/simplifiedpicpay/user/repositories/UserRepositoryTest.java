c44b2da721a1931f8e0d35bc144fb1f6ffcdf0ea
Vítor Lougon <vitor.lougon@gmail.com>
2025-11-15 19:11:19 -0300

    feat: Initializing unit tests

---
+++ picpay-api/src/test/java/br/com/simplifiedpicpay/user/repositories/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package br.com.simplifiedpicpay.user.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Test
+    void findUserByDocumentOrEmail() {
+    }
+}
